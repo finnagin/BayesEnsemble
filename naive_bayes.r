776ae8e12315c72d26e14844c1d96735b52d03cd
finnagin <flan313@gmail.com>
2020-02-26 18:03:55 -0800

    added sign function to all columns in the data frame

--- naive_bayes.r
+++ naive_bayes.r
@@ -10,6 +10,7 @@ file_dir <- here()
 csv_path <- file.path(file_dir,"tokenized_train_imdb.csv")
 
 df <- read.table(csv_path, header=TRUE, sep=',')
+df <- mutate_all(df, sign)
 df <- select(df, -X)
 df <- df[sample(nrow(df)),]
 df[colnames(df)] <- lapply(df[colnames(df)], factor)
@@ -23,8 +24,8 @@ nb <- naive.bayes(train, "class")
 
 fitted <- bn.fit(nb, train)
 
-pred <- predict(fitted, train, prob=TRUE)
+pred <- predict(fitted, test, prob=TRUE)
 
 n <- length(pred)
 
-
+sum(test[,'class'] == pred[1:n])/n
